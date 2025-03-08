6b9c294c6bf95455e40ea3386f30153aafbfab81
RaresFZ <rares.dragomir@epitech.eu>
2025-03-08 19:31:59 +0100

    added groovy

--- tests/test_main.c
+++ tests/test_main.c
@@ -1,14 +1,14 @@
 #include <criterion/criterion.h>
 
-/*
+
 Test(basic_test, passing_test) {
     cr_assert(1 == 1, "This test should pass");
 }
-*/
 
 // Uncomment this test to simulate a failing build
 
+/*
 Test(basic_test, failing_test) {
     cr_assert(1 == 0, "This test should fail");
 }
-
+*/
